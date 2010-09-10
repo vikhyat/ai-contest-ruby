4cf10f88f07d2e4be04e7360b455487e1a6be3ed
Vikhyat Korrapati <vikhyatk@gmail.com>
2010-09-10 13:00:40 +0530

    newer version of mybot.rb

--- mybot.rb
+++ mybot.rb
@@ -1,38 +1,31 @@
 require './planetwars.rb'
 
-def do_turn(pw)
-  # (1) don't do anything if we have a fleet in flight
-  return if pw.my_fleets.length >= 1
+def naive_strategy(pw)
+  # don't do anything if any of the following is true:
+  # (1) we have more than three fleets in flight
+  # (2) we don't have any planets left
+  # (3) we have captured every planet
+  return if pw.my_fleets.length >= 3
+  return if pw.my_planets.length == 0
+  return if pw.not_my_planets.length == 0
+  
+  # get a list of planets owned by me, sorted from weakest to strongest
+  my_planets = pw.my_planets.sort_by {|x| x.num_ships }
+  # get a list of planets that aren't mine, from strongest to weakest
+  other_planets = pw.not_my_planets.sort_by {|x| 1.0/(1+x.num_ships) }
 
-  # (2) find my strongest planet
-  source = -1
-  source_score = -999999.0
-  source_num_ships = 0
-  pw.my_planets.each do |planet|
-    score = planet.num_ships.to_f
-    if score > source_score
-      source_score = score
-      source = planet.planet_id
-      source_num_ships = planet.num_ships
-    end
-  end
-
-  # (3) find the weakest planet belonging that isn't mine
-  dest = -1
-  dest_score = -999999.0
-  pw.not_my_planets.each do |p|
-    score = 1.0 / (1 + p.num_ships)
-    if score > dest_score
-      dest_score = score
-      dest = p.planet_id
-    end
-  end
-
-  # (4) send half of the ships from my strongest planet to the weakest one that i do not own
-  if source >= 0 and dest >= 0
-    num_ships = source_num_ships / 2
+  # send half of the ships from my weakest planet to my strongest one
+  source = my_planets[-1].planet_id
+  source_ships = my_planets[-1].num_ships
+  dest = other_planets[-1].planet_id
+  if source >= 0 and dest >= 0 
+    num_ships = source_ships / 2
     pw.issue_order(source, dest, num_ships)
   end
+end
+
+def do_turn(pw)
+  naive_strategy(pw)
 end
 
 map_data = ''
